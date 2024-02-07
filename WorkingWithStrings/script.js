bb8248bf1321b740968b29526615576bee4a6ac5
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-02-07 19:19:11 +0200

    Working With Strings - Part 3

--- WorkingWithStrings/script.js
+++ WorkingWithStrings/script.js
@@ -92,7 +92,7 @@ console.log(new String("dima"));
 console.log(typeof new String("dima"));
 console.log(new String("dima").slice(1));
 console.log(typeof new String("dima").slice(1));
-*/
+
 
 // Part 2
 
@@ -155,3 +155,54 @@ const checkBaggage = function (items) {
 checkBaggage("I have a laptop, some food and a pocket Knife");
 checkBaggage("Socks and camera");
 checkBaggage("Got some snacks and a gun for protection");
+*/
+
+// Part 3
+
+// Split and join
+console.log("a+very+nice+string".split("+"));
+console.log("Dima Filonenko".split(" "));
+
+const [firstName, lastName] = "Dima Filonenko".split(" ");
+
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(" ");
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(" "));
+};
+
+capitalizeName("jessica ann smith davis");
+capitalizeName("dima filonenko");
+
+// Padding
+const message = "Go to gate 23!";
+console.log(message.padStart(20, "+").padEnd(30, "+"));
+console.log("Dima".padStart(20, "+").padEnd(30, "+"));
+
+const maskCreditCard = function (number) {
+  const str = number + "";
+  const last = str.slice(-4);
+  return last.padStart(str.length, "*");
+};
+
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard("154556864168520861"));
+
+// Repeat
+const message2 = "Bad weather... All Departures Delayed... ";
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${"✈".repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
